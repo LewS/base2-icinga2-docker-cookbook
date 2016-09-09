18e6d3626c83eb9954cb7adcdc39bc4fc20ec84e
LewS <lew@redgrid.net>
2016-09-09 11:32:42 +1000

    remove attempt to start docker from install.rb

--- recipes/install.rb
+++ recipes/install.rb
@@ -4,14 +4,14 @@ package 'libcgroup'
 
 unless node['base2']['environment']['platform'] == 'azure'
   service 'cgconfig' do
-    action [:enable, :start]
+    action [:enable]
   end
 end
 
 package "docker"
 
 service 'docker' do
-  action [:enable, :start]
+  action [:enable]
 end
 
 include_recipe "base2-icinga2-docker::build_container"
